cdc0de46ffaa737c4c74e055f8b1c37c334caf99
Nelson Gillo <nelson.gillo@tum.de>
2024-11-24 09:28:00 +0100

    debug OFF

--- src/main.py
+++ src/main.py
@@ -3,7 +3,5 @@ import logging
 import uvicorn
 from src.gateway.app import app
 
-logging.basicConfig(level=logging.DEBUG)
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
